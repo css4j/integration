ce06695e2f88bd6360dde98ff055fce261962ee0
Carlos Amengual <carlosame644+github@gmail.com>
2019-07-19 19:34:21 +0200

    Change Sourceforge URLs for Github's.

--- ci/io/github/css4j/ci/SampleSitesIT.java
+++ ci/io/github/css4j/ci/SampleSitesIT.java
@@ -5,7 +5,7 @@
  SPDX-License-Identifier: BSD-3-Clause
 
  Licensed under a BSD-style License. You can find the license here:
- https://carte.sourceforge.io/LICENSE.txt
+ https://css4j.github.io/LICENSE.txt
 
  */
 
